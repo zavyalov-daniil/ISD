d979960173619f883ffb31e06cbdaa19bad253cb
Daniil Zavyalov <dezavv1@gmail.com>
2022-11-06 22:57:40 +0600

    Так делать не стоит

--- src/main/java/com/notes/isd/repositories/UserAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.notes.isd.repositories;
-
-import com.notes.isd.entities.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
-
-}
