638a79f49b5d61d41dada9e1d7d71deb49f7a673
Kiselev Danil <dabikis2855@gmail.com>
2022-12-07 12:02:05 +0600

    [+] ExceptiondeGeneratorsController's controllers renamed and added controller to throw BadCredentials.

--- src/main/java/com/notes/isd/contollers/ExceptiondeGeneratorsController.java
+++ src/main/java/com/notes/isd/contollers/ExceptiondeGeneratorsController.java
@@ -1,12 +1,18 @@
 package com.notes.isd.contollers;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ExceptiondeGeneratorsController {
     @GetMapping("/gimme_error")
-    public String processRegister() {
+    public String generateRuntimeException() {
         throw new RuntimeException("Here we are!");
+    }
+
+    @GetMapping("/gimme_bad_credentials")
+    public String generateBadCredentials() {
+        throw new BadCredentialsException("Here we are!");
     }
 }
